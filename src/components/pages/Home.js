842193ee47389b0fa68b6855b7ea54a66439d709
Cyril Chukwudi Chidolue <cyrilchidolue2016@gmail.com>
2022-02-14 18:05:39 +0100

    Progressive Changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,21 +2,23 @@ import React from 'react';
 import Navbar from '../layout/Navbar';
 import HeroPage from '../layout/HeroPage';
 import Services from '../layout/Services';
+import LaptopVideo from '../layout/LaptopVideo';
 import OurPeople from '../layout/OurPeople';
 
 const Home = () => {
   return (
     <>
-    <Navbar/>
-    <section>
-      {/* HeroPage */}
-      <HeroPage />
+      <Navbar />
+      <section>
+        {/* HeroPage */}
+        <HeroPage />
 
-      {/* Services */}
-      <Services />
+        {/* Services */}
+        <Services />
 
-      {/* Our People, Our Culture */}
-      <OurPeople />
+        {/* Our People, Our Culture */}
+        <OurPeople />
+        <LaptopVideo />
       </section>
     </>
   );
