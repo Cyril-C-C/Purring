58d5b4189ebc422c0fe542a75a17360a928ce794
Cyril Chukwudi Chidolue <cyrilchidolue2016@gmail.com>
2022-11-29 02:43:18 +0100

    Final Changes

--- src/components/layout/MenuItems.js
+++ src/components/layout/MenuItems.js
@@ -1,32 +1,32 @@
 export const MenuItems = [
     {
       title: 'UI - UX Team',
-      path: '/ui-ux',
+      path: '/#',
       cName: 'dropdown-link'
     },
     {
       title: 'Design Team',
-      path: '/design',
+      path: '/#',
       cName: 'dropdown-link'
     },
     {
       title: 'Front End Team',
-      path: '/front-end',
+      path: '/#',
       cName: 'dropdown-link'
     },
     {
       title: 'Back End Team',
-      path: '/back-end',
+      path: '/#',
       cName: 'dropdown-link'
     },
     {
       title: 'CI - CD Team',
-      path: '/ci-cd',
+      path: '/#',
       cName: 'dropdown-link'
     },
     {
       title: 'Testing Team',
-      path: '/testing',
+      path: '/#',
       cName: 'dropdown-link'
     }
   ];
